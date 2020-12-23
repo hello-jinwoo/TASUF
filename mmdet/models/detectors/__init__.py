e0497d356443cbc0ace6153946d212e856326f54
Janghyuk Choi <jahyuky@gmail.com>
2020-12-23 17:21:01 +0900

    Edit /mmdet/models/detectors/__init__.py

--- mmdet/models/detectors/__init__.py
+++ mmdet/models/detectors/__init__.py
@@ -8,7 +8,11 @@ from .mask_rcnn import MaskRCNN
 from .cascade_rcnn import CascadeRCNN
 from .retinanet import RetinaNet
 
+from .mask_rcnn_suf import MaskRCNNSUF
+from .two_stage_suf import TwoStageDetectorSUF
+
 __all__ = [
     'BaseDetector', 'SingleStageDetector', 'TwoStageDetector', 'RPN',
-    'FastRCNN', 'FasterRCNN', 'MaskRCNN', 'CascadeRCNN', 'RetinaNet'
+    'FastRCNN', 'FasterRCNN', 'MaskRCNN', 'CascadeRCNN', 'RetinaNet',
+    'MaskRCNNSUF', 'TwoStageDetectorSUF'
 ]
